6f110aef2669fcd390b8cc44189a5b01c8baf51d
Axel75018 <axel.calvet@gmail.com>
2023-01-17 12:01:33 +0100

    Message erreur

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -183,7 +183,7 @@
             //                                                     EMAIL
             // ---------------------------------------------------------
             if ($this.attr("type") !== undefined && $this.attr("type").toLowerCase() === "email") {
-              message = "Not a valid email address<!-- data-validator-validemail-message to override -->";
+              message = "Cette adresse mail est incorrecte<!-- data-validator-validemail-message to override -->";
               if ($this.data("validationValidemailMessage")) {
                 message = $this.data("validationValidemailMessage");
               } else if ($this.data("validationEmailMessage")) {
@@ -836,7 +836,7 @@
 			required: {
 				name: "Required",
 				type: "required",
-				message: "This is required<!-- data-validator-required-message to override -->"
+				message: "Veuillez renseigner ce champ<!-- data-validator-required-message to override -->"
 			},
 			checkone: {
 				name: "Checkone",
